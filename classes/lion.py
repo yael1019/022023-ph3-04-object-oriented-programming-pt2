fa400e26cb8c8281da3eb366e862f512185a99b7
Yael Harosh <yaelharosh01@gmail.com>
2023-04-06 16:42:51 -0400

    notes

--- classes/lion.py
+++ classes/lion.py
@@ -1,7 +1,15 @@
 # from custom_decorator.my_decorator import my_decorator
 from classes.big_cat import BigCat
+from custom_decorator.my_decorator import my_decorator
 
 class Lion(BigCat):
+    # Class variable/attribute 
+    # this will be directly attached to the class itself 
+    # if you run Lion.proper_lion_names it will return the list
+    # you can also use it in each instance
+    proper_lion_names = ['Bill', 'Ted', 'Sharief', 'Patty']
+    loudest_lion = None
+    all_the_lions = []
 
     def __init__(self, name, recent_food, mane):
         # super method represents the parent, the superclass
@@ -11,9 +19,26 @@ class Lion(BigCat):
         super().__init__(name, recent_food)
         self.mane = mane
         print('We are leaving Lion')
+        # we do Lion instead of self to be safe so it always affects the class not just the instance 
+        Lion.add_lion(self)
 
+    # to make somthing a class method we need to use a decorator
+    # decorator adds special functionality to the function
+    # decorators use pie syntax
+    # we pass in cls instead self, it is the self equivilant to self
+    # our instances can use these methods aswell
+    @classmethod
+    def list_names(cls):
+        return [name + 'the Lion' for name in cls.proper_lion_names]
+
+    @classmethod
+    def add_lion(cls, lion):
+        cls.all_the_lions.append(lion)
+
+    # this is an instance method, not a class method
     # you can call this function eat, but you do not have to, it does not have to match the superclass function
     # it does not have to be the same name if you want to access that super eat fn
+    @my_decorator
     def eat_foodies(self, food):
         super().eat(food)
         print('It was delishhhh')
@@ -23,3 +48,22 @@ class Lion(BigCat):
 
 bill = Lion('Bill', 'hamster', 'BIG')
 # => <Lion name = Bill recent_food = hamster mane = BIG >
+
+bill.proper_lion_names.append('George')
+# => ['Bill', 'Ted', 'Sharief', 'Patty', 'George]
+Lion.proper_lion_names.append('George')
+# => ['Bill', 'Ted', 'Sharief', 'Patty', 'George]
+# this persists because append is destrusctive  
+
+bill.loudest_lion = bill
+bill.loudest_lion
+# => <Lion name = Bill ... >
+Lion.loudest_lion
+# => None
+
+bill.list_names()
+# => ['Bill the Lion', 'Ted the Lion', 'Sharief the Lion', 'Patty the Lion']
+
+Lion.list_names()
+# ['Bill the Lion', 'Ted the Lion', 'Sharief the Lion', 'Patty the Lion']
+
--- custom_decorator/my_decorator.py
+++ custom_decorator/my_decorator.py
@@ -1,10 +1,34 @@
 def my_decorator(func):
+    # make and return a func inside of decorator 
+    # *args is the arguments, we want to be able to pass in any number amount of args
+    # and **kargs are key args
+        # when you invoke the function and pass in a key with a value it is a key arg
+        # whatever(name = 'bob')
     def wrapper(*args, **kwargs):
         print("Decorator is happening")
+        # pass in the *args and **kargs to the function 
+        # in our lion class we are calling the wrapper function passing in 2 arguments
         return func(*args, **kwargs)
         print("Decorator is happening again")
     return wrapper
 
+# this pie operator will pass in whatever into the my_decorator 
+# so when it calls func(), it will call whatever()
+# to run this you call whatever(), the decorator causes this to call my_decorator passing in whatever as a cb
+@my_decorator
+def whatever():
+    print('whatever')
+
+# the pie tag needs to match the title of the function we declared 
+@my_decorator
+def something():
+    print('something')
+
+# the long way of making a deocrator
+def another_func():
+    print('I am another func')
+
+another_func2 = my_decorator(another_func)
 # this has everything to do with the fact that python function are 'first class functions'
 # once we define a function we can pass it like any common variable
 
